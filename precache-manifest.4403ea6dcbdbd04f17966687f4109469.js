016d851a46dd15b61e63462cd649675621891ac3
Glievoyss <glievoy.s@gmail.com>
2019-12-16 06:08:23 +0200

    Updates

---
+++ precache-manifest.4403ea6dcbdbd04f17966687f4109469.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da930b8835768349bc06467b003df6e4",
+    "url": "/Mauris_tv_look/./index.html"
+  },
+  {
+    "revision": "329a6597a57af6ba2b0d",
+    "url": "/Mauris_tv_look/./static/css/2.e62aa5e7.chunk.css"
+  },
+  {
+    "revision": "f1ba776fd899375708de",
+    "url": "/Mauris_tv_look/./static/css/main.50889364.chunk.css"
+  },
+  {
+    "revision": "329a6597a57af6ba2b0d",
+    "url": "/Mauris_tv_look/./static/js/2.be534487.chunk.js"
+  },
+  {
+    "revision": "f1ba776fd899375708de",
+    "url": "/Mauris_tv_look/./static/js/main.55df1cfb.chunk.js"
+  },
+  {
+    "revision": "3c0a75b8eb01239ce60a",
+    "url": "/Mauris_tv_look/./static/js/runtime-main.9a1e51f5.js"
+  },
+  {
+    "revision": "6b89ac7a3713a98f30bacbf211cd46d1",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-300.6b89ac7a.woff"
+  },
+  {
+    "revision": "8f3de65e5114fcb6be3d3c90f550a3c9",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-300.8f3de65e.woff2"
+  },
+  {
+    "revision": "62ed774eab1afc41cdf6e8dd76060e27",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-300italic.62ed774e.woff2"
+  },
+  {
+    "revision": "7ae712706e15eb07ec5334db6b7ce57a",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-300italic.7ae71270.woff"
+  },
+  {
+    "revision": "454a161f247d452d0d752340f15d1c1f",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-600.454a161f.woff2"
+  },
+  {
+    "revision": "9803e73e4eac551ba5214aa16c593d61",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-600.9803e73e.woff"
+  },
+  {
+    "revision": "1b0f5fd914d3de3d4c96b72d091288d5",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-600italic.1b0f5fd9.woff"
+  },
+  {
+    "revision": "bb3b5b1f4abf864498c928d9797bc4de",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-600italic.bb3b5b1f.woff2"
+  },
+  {
+    "revision": "037a987eb96f3b85193e66a8e63d20c6",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-700.037a987e.woff"
+  },
+  {
+    "revision": "80c1f218918df17a6b8807231259f817",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-700.80c1f218.woff2"
+  },
+  {
+    "revision": "9e210023963814acc98204785902d7ac",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-700italic.9e210023.woff2"
+  },
+  {
+    "revision": "f235cf28af1f965c49e670dfae3efb86",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-700italic.f235cf28.woff"
+  },
+  {
+    "revision": "c3557f4dd812304312ff43097b172c79",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-800.c3557f4d.woff2"
+  },
+  {
+    "revision": "e58f8549a43a4b28bec3e9fab580d2f5",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-800.e58f8549.woff"
+  },
+  {
+    "revision": "e99b562ffa6ac1fe7d21c101b0efbbb5",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-800italic.e99b562f.woff2"
+  },
+  {
+    "revision": "f7db2eb707d40cdfbdbc44f6b89a4e96",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-800italic.f7db2eb7.woff"
+  },
+  {
+    "revision": "12142bd2abfe85cac139c98b384b1c25",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-italic.12142bd2.woff"
+  },
+  {
+    "revision": "5e1868ea3e7783969c58357a8cd7cbc7",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-italic.5e1868ea.woff2"
+  },
+  {
+    "revision": "9630afe934261d706e33f559f1f0a296",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-regular.9630afe9.woff"
+  },
+  {
+    "revision": "e45f098427277d1ac42ea15dc3e8e07f",
+    "url": "/Mauris_tv_look/./static/media/open-sans-v17-latin_cyrillic-ext_cyrillic-regular.e45f0984.woff2"
+  },
+  {
+    "revision": "9fde5658411fc9743ab8c7a28bac95ce",
+    "url": "/Mauris_tv_look/./static/media/tv.9fde5658.png"
+  }
+]);
