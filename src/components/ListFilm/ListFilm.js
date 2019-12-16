0abf4909703e142a323c5cb221240b1133bd0c47
Glievoyss <glievoy.s@gmail.com>
2019-12-16 05:32:03 +0200

    add button for all list films

--- src/components/ListFilm/ListFilm.js
+++ src/components/ListFilm/ListFilm.js
@@ -1,31 +1,51 @@
 /*eslint-disable*/
 
-import React from 'react';
+import React, { Component } from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import styles from './ListFilm.module.css';
 import CardFilm from '../CardFilm/CardFilm';
 
-const ListFilm = ({ listCinema, date, changeImgOriginal }) => {
-  return (
-    <>
-      <h2 className={styles.dataList}>{moment(date).format('D MMMM YYYY')}</h2>
-      <ul className={styles.filmList}>
-        {listCinema.map(({ id, show, name, number, season, airdate }) => (
-          <CardFilm
-            key={id}
-            show={show}
-            name={name}
-            number={number}
-            season={season}
-            airdate={airdate}
-            changeImgOriginal={changeImgOriginal}
-          />
-        ))}
-      </ul>
-    </>
-  );
-};
+class ListFilm extends Component {
+  state = {
+    listLenght: 2,
+  };
+  render() {
+    const { listCinema, date, changeImgOriginal } = this.props;
+    const { listLenght } = this.state;
+    return (
+      <>
+        <h2 className={styles.dataList}>
+          {moment(date).format('D MMMM YYYY')}
+        </h2>
+        <ul className={styles.filmList}>
+          {listCinema.map(
+            ({ id, show, name, number, season, airdate }, index) => (
+              <>
+                {index < listLenght && (
+                  <CardFilm
+                    key={id}
+                    show={show}
+                    name={name}
+                    number={number}
+                    season={season}
+                    airdate={airdate}
+                    changeImgOriginal={changeImgOriginal}
+                  />
+                )}
+              </>
+            ),
+          )}
+        </ul>
+        {listLenght === 2 && listCinema.length > 2 && (
+          <button className={styles.allListButton} type="button">
+            Еще {listCinema.length - 2} сериалов
+          </button>
+        )}
+      </>
+    );
+  }
+}
 
 // PhotoCard.propTypes = {
 //   webformatURL: PropTypes.string.isRequired,
